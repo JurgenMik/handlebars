8655b7a4f2a9a0423e9d8aac1b5703a0074164ac
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-02-03 15:59:07 +0200

    Displayed author by slug

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) =>{
 // Show article by this slug
 
 app.get('/article/:slug', (req,res) =>{
-    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`
+    let query = `SELECT * , author.name as author_name, article.name as article_name FROM author  iNNER JOIN article ON author.id = article.author_id WHERE slug="${req.params.slug}"`
     let article
     con.query(query, (err,result) => {
         if (err) throw err;
