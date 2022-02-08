f7bf88e21e1dc94e06dde13823cf430285545d2d
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-02-08 18:55:16 +0200

    Created author controller and route, refactored application code

--- controllers/article.js
+++ controllers/article.js
@@ -2,7 +2,7 @@
 const con = require('../utils/db');
 
 // Show all articles - index page
-const getAllArticles = (req, res) =>{
+const getAllArticles = (req, res) => {
     let query ="SELECT * FROM article";
     let articles = []
     con.query(query, (err, result) => {
@@ -16,7 +16,7 @@ const getAllArticles = (req, res) =>{
 
 // Show article by this slug
 
-const getArticleBySlug = (req,res) =>{
+const getArticleBySlug = (req,res) => {
     let query = `SELECT * , author.name as author_name, article.name as article_name FROM author  iNNER JOIN article ON author.id = article.author_id WHERE slug="${req.params.slug}"`
     let article
     con.query(query, (err,result) => {
